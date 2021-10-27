d6593332e80be3d57d43754a7b11a7f2b529076b
steve.qiu <steveqiu@outlook.com>
2021-10-27 10:35:33 -0700

    get the node running
    node server

--- app/app.js
+++ app/app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongodbService = require('./service/mongodb/mongodbService.js')
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/*', (req, res) => {
+  res.send('other route')
 })
 
 app.listen(port, () => {
