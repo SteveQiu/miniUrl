ba4cb4e85a0f0221a565f45d89bb665feb7f19e3
Arshdeep Bhullar <bhullar.arsh7@gmail.com>
2021-10-28 10:16:11 -0700

    added hash approach

--- app/app.js
+++ app/app.js
@@ -2,16 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 const mongodbService = require('./service/mongodb/mongodbService.js')
-
+app.use(express.json())
 
 app.get('/list', (req, res) => {
   const list = mongodbService.listAll()
   // console.log(list)
-  // res.send('list all')
-  res.send(list)
+  res.send('list all')
+  // res.send(list)
 })
 app.get('/save', (req, res) => {
-  mongodbService.save('www.article2.com')
+  mongodbService.save('www.article7.com')
+  // mongodbService.save('www.article6.com')
   res.send('save')
 })
 app.get('/', (req, res) => {
--- app/service/mongodb/mongodbService.js
+++ app/service/mongodb/mongodbService.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Pair = require('./schema/Pair');
+const hashGenerator = require('./hashGenerator.js');
 // main().catch(err => console.log(err));
 
 mongoose.connect('mongodb://localhost:27017/miniUrl');
@@ -11,8 +12,18 @@ module.exports = {
     },
     save(url){
         console.log(mongoose.connection.readyState);
-        const newPair = new Pair({key:Math.random(), value:url})
-        newPair.save()
+        console.log("Generated Hash:")
+        // hashKey = null
+        hashKey = hashGenerator.generateHash(url);
+        (async () => {Pair.findOne({key: hashKey}).then(function(doc){
+            if(doc != null){
+                console.log("Hash already exists");
+                console.log("doc", Object.keys(doc).length, doc);
+                hashKey = hashGenerator.generateHash(url+"_"+Math.random());
+            }   
+                // const newPair = new Pair({key:hashKey, value:url})
+                // newPair.save()
+        });})();
     },
     get(key){
 
